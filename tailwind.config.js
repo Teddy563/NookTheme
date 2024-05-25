1ec5d20563b62b0d30ed3e097b5c5f2ca0b6681a
Teddy563 <tediteditu511@gmail.com>
2024-05-26 01:33:42 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,17 +20,30 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                header: ['"IBM Plex Sans"', '"Roboto"', 'system-ui', 'sans-serif'],
+                header: ['"Inter"', '"Inter"', 'system-ui', 'sans-serif'],
             },
             colors: {
                 black: '#131a20',
                 // "primary" and "neutral" are deprecated, prefer the use of "blue" and "gray"
                 // in new code.
                 primary: colors.blue,
-                orange: colors.orange,
+                orange: gray,
                 gray: gray,
                 neutral: gray,
                 cyan: colors.cyan,
+                orange: {
+                    50: '#fffbeb',
+                    100: '#fef3c7',
+                    200: '#fde68a',
+                    300: '#fcd34d',
+                    400: '#fbbf24',
+                    500: '#f59e0b',
+                    600: '#d97706',
+                    700: '#b45309',
+                    800: '#92400e',
+                    900: '#78350f',
+                    950: '#451a03',
+                },
                 neutral: {
                     50: colors.neutral[50],
                     100: colors.neutral[100],
